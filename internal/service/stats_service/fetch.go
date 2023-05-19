249b9090e8074f1a183c211823a8e2dad7879d81
alexadastra <semenchenkoaleksey92@gmail.com>
2023-05-19 14:22:09 +0300

    [dev] fix findWeekRange and make process all the locations

--- internal/service/stats_service/fetch.go
+++ internal/service/stats_service/fetch.go
@@ -33,10 +33,16 @@ func (s *StatsService) FetchStats(ctx context.Context, userID int64) (int64, int
 	return int64(len(checkins)), int64(len(gratitudes)), nil
 }
 
-// suppose that t has UTC location
 func findWeekRange(t time.Time) (time.Time, time.Time) {
+	tUTC := t.UTC()
+
+	// timeZoneDiff = t - tUTC in nanoseconds (positive for zones to the east from Greenwich, negative - for zones to the west)
+	_, locOffset := t.Zone()
+	_, utcOffset := tUTC.Zone()
+	timeZoneDiff := time.Duration((locOffset - utcOffset) * 1000000000)
+
 	// extract the day of week (0 for Sunday, 1 for Monday, ..., 6 for Saturday)
-	currentDay := int(t.Weekday())
+	currentDay := int(tUTC.Weekday())
 
 	// handle Sunday as i want the week to be started from Monday
 	if currentDay == 0 {
@@ -44,9 +50,19 @@ func findWeekRange(t time.Time) (time.Time, time.Time) {
 	}
 
 	// whenever currentDay is, if we count now() - (currentDay - 1) days, it'll always point to Monday
-	fromDay := t.Add((time.Duration(currentDay) - 1) * 24 * time.Hour)
+	fromDayUTC := tUTC.Add(-1 * (time.Duration(currentDay) - 1) * 24 * time.Hour)
 
-	fromDay = fromDay.Truncate(24 * time.Hour)
+	// set time to UTC midnight
+	fromDayUTC = fromDayUTC.Truncate(24 * time.Hour)
 
-	return fromDay, fromDay.Add(7 * 24 * time.Hour)
+	// fromDayUTCDiff = tUTC - fromDay (at least zero)
+	fromDayUTCDiff := tUTC.Sub(fromDayUTC)
+
+	// extract found difference between current UTC and monday midnight UTC
+	fromDayLoc := t.Add(-1 * fromDayUTCDiff)
+
+	// extract difference between time zones, as we truncated the UTC time
+	fromDayLoc = fromDayLoc.Add(-1 * timeZoneDiff)
+
+	return fromDayLoc, fromDayLoc.Add(7 * 24 * time.Hour)
 }
--- internal/service/stats_service/fetch_test.go
+++ internal/service/stats_service/fetch_test.go
@@ -1,7 +1,6 @@
 package stats_service
 
 import (
-	"reflect"
 	"testing"
 	"time"
 )
@@ -11,27 +10,27 @@ func Test_findWeekRange(t *testing.T) {
 		t time.Time
 	}
 	tests := []struct {
-		name  string
-		args  args
-		wantFrom  time.Time
-		wantTo time.Time
+		name     string
+		args     args
+		wantFrom time.Time
+		wantTo   time.Time
 	}{
 		{
 			name: "wednesday",
 			args: args{
-				t: time.Unix(1684331504, 0), 	// Wed May 17 2023 13:51:44 GMT+0000
+				t: time.Unix(1684331504, 0), // Wed May 17 2023 13:51:44 GMT+0000
 			},
-			wantFrom: time.Unix(1684011600, 0), // Sat May 13 2023 21:00:00 GMT+0000
-			wantTo: time.Unix(1684702800, 0), 	// Sun May 21 2023 21:00:00 GMT+0000
+			wantFrom: time.Unix(1684098000, 0), // Sun May 14 2023 21:00:00 GMT+0000
+			wantTo:   time.Unix(1684702800, 0), // Sun May 21 2023 21:00:00 GMT+0000
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, got1 := findWeekRange(tt.args.t)
-			if !reflect.DeepEqual(got, tt.wantFrom) {
+			if got.Unix() != tt.wantFrom.Unix() {
 				t.Errorf("findWeekRange() got = %v, want %v", got, tt.wantFrom)
 			}
-			if !reflect.DeepEqual(got1, tt.wantTo) {
+			if got1.Unix() != tt.wantTo.Unix() {
 				t.Errorf("findWeekRange() got1 = %v, want %v", got1, tt.wantTo)
 			}
 		})
